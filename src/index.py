e77d0663fc713c83326173163b72454d84e42647
Nick Wang <wang703003515@gmail.com>
2021-02-18 10:24:43 +0000

    implmented regex to set wild_mode

--- src/index.py
+++ src/index.py
@@ -1,12 +1,15 @@
 import sys
 from recipe_scrapers import scrape_me
 import json
+from checkUrl import checkUrl
 
 
 class Scraper:
-    def __init__(self, url, wild_mode):
+    def __init__(self, url):
+        wild_mode = checkUrl(url)
+        print("using wild_mode", wild_mode)
         # todo: run regex to check if the input url is in the predefined supported sites
-        result = scrape_me(url, wild_mode=True if wild_mode else False)
+        result = scrape_me(url, wild_mode=wild_mode)
 
         self.title = result.title()
         self.total_time = result.total_time()
@@ -20,7 +23,8 @@ class Scraper:
 
 
 url = sys.argv[1]
-wild_mode = sys.argv[2]
-result = Scraper(url, wild_mode)
+# wild_mode = sys.argv[2]
+# result = Scraper(url, wild_mode)
+result = Scraper(url)
 print(json.dumps(vars(result)))
 # print(json.dumps(vars(result), sort_keys=True, indent=4))
